29053c616a060c51b92afce4139024f77bcbc08f
chow <zhouyi_zhou@foxmail.com>
2023-10-24 23:29:45 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/products">商品列表</router-link>
+      <router-link to="/cart">购物车</router-link>
+      <router-link to="/profile">个人信息</router-link>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+html, body {
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  padding: 0 200px;
 }
 
 nav {
@@ -30,3 +40,5 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup>
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <div class="navbar">
+      <router-link to="/products">商品列表</router-link>
+      <router-link to="/cart">购物车</router-link>
+      <router-link to="/profile">个人信息</router-link>
+    </div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+import list from '@/views/merchandise/list.vue'
+export default {
+  components: {
+    list
+  },
+  data() {
+    return {
+      activeName: 'second'
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+}
+</script>
+<style scoped>
+.about {
+  padding: 0 200px;
+}
+</style>
